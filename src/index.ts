dd32449089a6d7bee749cb0dde1e5675ebd108bb
tejaskh3 <tejaskh3@gmail.com>
2024-03-05 16:50:10 +0530

    feat: add prisma and define routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("signup");
+});
+app.post("/api/v1/user/login", (c) => {
+  return c.text("login");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("create blog");
+});
+
+app.patch("/api/v1/blog", (c) => {
+  return c.text("update blog");
+});
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get a blog");
+});
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("create bulk blogs");
+});
+app.delete("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("delete blog");
+});
+export default app;
